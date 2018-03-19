02aaacf1450dcd82fc388d0c4e1480ccfa31255e
Habib Ali <admin@admins-mac-mini.local>
2018-03-19 06:46:47 +0500

    create basic tableView and displayed hardcoded data from array in the table cells

---
+++ Todoey/TodoListVC.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Admin on 2018-03-19.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class TodoListVC: UITableViewController {
+
+    let itemArray = ["Learn iOS", "Learn Android", "Learn Django"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+
+}
+
